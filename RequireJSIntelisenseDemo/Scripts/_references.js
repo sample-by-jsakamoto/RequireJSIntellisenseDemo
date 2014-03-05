d40e587b02a6d76700c99d67e588149b5fb649f4
jsakamoto <junichi.jp@gmail.com>
2014-03-05 21:06:35 +0900

    1st step.

---
+++ RequireJSIntelisenseDemo/Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.1.0.js" />
+/// <reference path="../demo.js" />
+/// <reference path="require.js" />
+/// <reference path="r.js" />
---
+++ RequireJSIntelisenseDemo/demo.js
@@ -0,0 +1,6 @@
+﻿define(["jquery", "ko"],
+    function ($, ko) {
+        // Type '$' and '.' here in Visual Studio JavaScript editor.
+        // What do you see?
+        
+    });
